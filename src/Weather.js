ac81f00fdf15d1dfd881404b4a6aa86c243c5fed
josevaltier <84319890+josevaltier@users.noreply.github.com>
2021-08-26 11:09:11 +0200

    Added css to Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -5,23 +5,32 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input type="search" placeholder="Type a city name..." />
-        <input type="submit" value="Go!" />
-        <input type="submit" value="📍" />
+        <input
+          type="search"
+          placeholder="Where to?"
+          autoFocus="on"
+          className="searchBox"
+        />
+        <input type="submit" value="Go!" className="searchButton" />
+        <input type="submit" value="📍" className="locateMeButton" />
       </form>
 
       <h1>Barcelona</h1>
       <h3>Wednesday, August 25th</h3>
 
       <div className="row">
-        <div className="col-6">
-          <img src="./sunny.png" /> <span>30ºC</span>
+        <div className="col-12 currentWeather">
+          <img src="./sunny.png" alt="sunny" />{" "}
+          <span>
+            <span className="mainTemp">30</span>
+            <span className="unit">ºC</span>
+          </span>
         </div>
       </div>
-      <div>Min 23ºC | Max 30ºC</div>
-      <div className="row">
-        <div className="col-3">Humidity: 50%</div>
-        <div className="col-3">Wind: 3 km/h</div>
+      <div className="minMaxTemp">Min 23ºC | Max 30ºC</div>
+      <div className="row mt-3">
+        <div className="col-5 humidity">Humidity: 50%</div>
+        <div className="col-5 wind">Wind: 3 km/h</div>
       </div>
     </div>
   );
